3be61a49bcf6899a50f19760a468b32d0dd3f2bb
Moran <morika.b@gmail.com>
2022-02-13 13:33:03 +0200

    Initial attempt in analyzing results of selective promoters

--- modules/main.py
+++ modules/main.py
@@ -20,8 +20,8 @@ logger = LoggerFactory.create_logger("main")
 
 current_directory = Path(__file__).parent.resolve()
 # base_path = os.path.join(Path(current_directory).parent.resolve(), "example_data")
-base_path = "/arabidopsis_microbiome"
-# base_path = "C:\\Users\\Kama\\Documents\\Moran\\biomedical-engineering\\microbiome-optimization\\arabidopsis_microbiome"
+# base_path = "/arabidopsis_microbiome"
+base_path = "C:\\Users\\Kama\\Documents\\Moran\\biomedical-engineering\\microbiome-optimization\\arabidopsis_microbiome"
 
 default_user_inp_raw = {
     'sequence': os.path.join(base_path, 'mCherry_original.fasta'),
@@ -36,134 +36,134 @@ default_user_inp_raw = {
                  'optimized': False,
                  'expression_csv': None,
                  },
-        'org3': {'genome_path': os.path.join(base_path, 'Arthrobacter luteolus.gb'),
-                 'optimized': False,
-                 'expression_csv': None,
-                 },
-        'org4': {'genome_path': os.path.join(base_path, 'Arthrobacter pascens.gb'),
-                 'optimized': False,
-                 'expression_csv': None,
-                 },
-        'org5': {'genome_path': os.path.join(base_path, 'Arthrobacter subterraneus.gb'),
-                 'optimized': False,
-                 'expression_csv': None,
-                 },
-        'org6': {'genome_path': os.path.join(base_path, 'Arthrobacter tumbae.gb'),
-                 'optimized': False,
-                 'expression_csv': None,
-                 },
-        'org7': {'genome_path': os.path.join(base_path, 'Brevibacterium frigoritolerans.gb'),
-                 'optimized': False,
-                 'expression_csv': None,
-                 },
-        'org8': {'genome_path': os.path.join(base_path, 'Janibacter limosus.gb'),
-                 'optimized': False,
-                 'expression_csv': None,
-                 },
-        'org9': {'genome_path': os.path.join(base_path, 'Knoellia subterranea.gb'),
-                 'optimized': False,
-                 'expression_csv': None,
-                 },
-        'org10': {'genome_path': os.path.join(base_path, 'Mycolicibacterium smegmatis.gb'),
-                 'optimized': False,
-                 'expression_csv': None,
-                 },
-        'org11': {'genome_path': os.path.join(base_path, 'Nocardioides daejeonensis.gb'),
-                 'optimized': False,
-                 'expression_csv': None,
-                 },
-        'org12': {'genome_path': os.path.join(base_path, 'Nocardioides jensenii.gb'),
-                 'optimized': False,
-                 'expression_csv': None,
-                 },
-        'org13': {'genome_path': os.path.join(base_path, 'Nocardioides oleivorans.gb'),
-                 'optimized': False,
-                 'expression_csv': None,
-                 },
-        'org14': {'genome_path': os.path.join(base_path, 'Nocardioides sediminis.gb'),
-                 'optimized': False,
-                 'expression_csv': None,
-                 },
-        'org15': {'genome_path': os.path.join(base_path, 'Nocardioides terrigena.gb'),
-                 'optimized': False,
-                 'expression_csv': None,
-                 },
-        'org16': {'genome_path': os.path.join(base_path, 'Paenarthrobacter nitroguajacolicus.gb'),
-                 'optimized': False,
-                 'expression_csv': None,
-                 },
-        'org17': {'genome_path': os.path.join(base_path, 'Paenibacillus aceris.gb'),
-                 'optimized': False,
-                 'expression_csv': None,
-                 },
-        'org18': {'genome_path': os.path.join(base_path, 'Paenibacillus alginolyticus.gb'),
-                 'optimized': False,
-                 'expression_csv': None,
-                 },
-        'org19': {'genome_path': os.path.join(base_path, 'Paenibacillus oryzisoli.gb'),
-                 'optimized': False,
-                 'expression_csv': None,
-                 },
-        'org20': {'genome_path': os.path.join(base_path, 'Paenibacillus prosopidis.gb'),
-                 'optimized': False,
-                 'expression_csv': None,
-                 },
-        'org21': {'genome_path': os.path.join(base_path, 'Paenibacillus qinlingensis.gb'),
-                 'optimized': False,
-                 'expression_csv': None,
-                 },
-        'org22': {'genome_path': os.path.join(base_path, 'Pedococcus badiiscoriae.gb'),
-                 'optimized': False,
-                 'expression_csv': None,
-                 },
-        'org23': {'genome_path': os.path.join(base_path, 'Pedococcus bigeumensis.gb'),
-                  'optimized': False,
-                  'expression_csv': None,
-                  },
-        'org24': {'genome_path': os.path.join(base_path, 'Pedococcus dokdonensis.gb'),
-                  'optimized': False,
-                  'expression_csv': None,
-                  },
-        'org25': {'genome_path': os.path.join(base_path, 'Peribacillus muralis.gb'),
-                  'optimized': False,
-                  'expression_csv': None,
-                  },
-        'org26': {'genome_path': os.path.join(base_path, 'Peribacillus simplex.gb'),
-                  'optimized': False,
-                  'expression_csv': None,
-                  },
-        'org27': {'genome_path': os.path.join(base_path, 'Phycicoccus duodecadis.gb'),
-                  'optimized': False,
-                  'expression_csv': None,
-                  },
-        'org28': {'genome_path': os.path.join(base_path, 'Priestia flexa.gb'),
-                  'optimized': False,
-                  'expression_csv': None,
-                  },
-        'org29': {'genome_path': os.path.join(base_path, 'Pseudarthrobacter phenanthrenivorans.gb'),
-                  'optimized': False,
-                  'expression_csv': None,
-                  },
-        'org30': {'genome_path': os.path.join(base_path, 'Rhodanobacter denitrificans.gb'),
-                  'optimized': False,
-                  'expression_csv': None,
-                  },
-        'org31': {'genome_path': os.path.join(base_path, 'Rhodanobacter fulvus.gb'),
-                  'optimized': False,
-                  'expression_csv': None,
-                  },
-        'org32': {'genome_path': os.path.join(base_path, 'Terrabacter aerolatus.gb'),
-                  'optimized': False,
-                  'expression_csv': None,
-                  },
-        'org33': {'genome_path': os.path.join(base_path, 'Terrabacter tumescens.gb'),
-                  'optimized': False,
-                  'expression_csv': None,
-                  },
-        'org34': {'genome_path': os.path.join(base_path, 'Yonghaparkia alkaliphila.gb'),
-                  'optimized': False,
-                  'expression_csv': None,
-                  },
+        # 'org3': {'genome_path': os.path.join(base_path, 'Arthrobacter luteolus.gb'),
+        #          'optimized': False,
+        #          'expression_csv': None,
+        #          },
+        # 'org4': {'genome_path': os.path.join(base_path, 'Arthrobacter pascens.gb'),
+        #          'optimized': False,
+        #          'expression_csv': None,
+        #          },
+        # 'org5': {'genome_path': os.path.join(base_path, 'Arthrobacter subterraneus.gb'),
+        #          'optimized': False,
+        #          'expression_csv': None,
+        #          },
+        # 'org6': {'genome_path': os.path.join(base_path, 'Arthrobacter tumbae.gb'),
+        #          'optimized': False,
+        #          'expression_csv': None,
+        #          },
+        # 'org7': {'genome_path': os.path.join(base_path, 'Brevibacterium frigoritolerans.gb'),
+        #          'optimized': False,
+        #          'expression_csv': None,
+        #          },
+        # 'org8': {'genome_path': os.path.join(base_path, 'Janibacter limosus.gb'),
+        #          'optimized': False,
+        #          'expression_csv': None,
+        #          },
+        # 'org9': {'genome_path': os.path.join(base_path, 'Knoellia subterranea.gb'),
+        #          'optimized': False,
+        #          'expression_csv': None,
+        #          },
+        # 'org10': {'genome_path': os.path.join(base_path, 'Mycolicibacterium smegmatis.gb'),
+        #          'optimized': False,
+        #          'expression_csv': None,
+        #          },
+        # 'org11': {'genome_path': os.path.join(base_path, 'Nocardioides daejeonensis.gb'),
+        #          'optimized': False,
+        #          'expression_csv': None,
+        #          },
+        # 'org12': {'genome_path': os.path.join(base_path, 'Nocardioides jensenii.gb'),
+        #          'optimized': False,
+        #          'expression_csv': None,
+        #          },
+        # 'org13': {'genome_path': os.path.join(base_path, 'Nocardioides oleivorans.gb'),
+        #          'optimized': False,
+        #          'expression_csv': None,
+        #          },
+        # 'org14': {'genome_path': os.path.join(base_path, 'Nocardioides sediminis.gb'),
+        #          'optimized': False,
+        #          'expression_csv': None,
+        #          },
+        # 'org15': {'genome_path': os.path.join(base_path, 'Nocardioides terrigena.gb'),
+        #          'optimized': False,
+        #          'expression_csv': None,
+        #          },
+        # 'org16': {'genome_path': os.path.join(base_path, 'Paenarthrobacter nitroguajacolicus.gb'),
+        #          'optimized': False,
+        #          'expression_csv': None,
+        #          },
+        # 'org17': {'genome_path': os.path.join(base_path, 'Paenibacillus aceris.gb'),
+        #          'optimized': False,
+        #          'expression_csv': None,
+        #          },
+        # 'org18': {'genome_path': os.path.join(base_path, 'Paenibacillus alginolyticus.gb'),
+        #          'optimized': False,
+        #          'expression_csv': None,
+        #          },
+        # 'org19': {'genome_path': os.path.join(base_path, 'Paenibacillus oryzisoli.gb'),
+        #          'optimized': False,
+        #          'expression_csv': None,
+        #          },
+        # 'org20': {'genome_path': os.path.join(base_path, 'Paenibacillus prosopidis.gb'),
+        #          'optimized': False,
+        #          'expression_csv': None,
+        #          },
+        # 'org21': {'genome_path': os.path.join(base_path, 'Paenibacillus qinlingensis.gb'),
+        #          'optimized': False,
+        #          'expression_csv': None,
+        #          },
+        # 'org22': {'genome_path': os.path.join(base_path, 'Pedococcus badiiscoriae.gb'),
+        #          'optimized': False,
+        #          'expression_csv': None,
+        #          },
+        # 'org23': {'genome_path': os.path.join(base_path, 'Pedococcus bigeumensis.gb'),
+        #           'optimized': False,
+        #           'expression_csv': None,
+        #           },
+        # 'org24': {'genome_path': os.path.join(base_path, 'Pedococcus dokdonensis.gb'),
+        #           'optimized': False,
+        #           'expression_csv': None,
+        #           },
+        # 'org25': {'genome_path': os.path.join(base_path, 'Peribacillus muralis.gb'),
+        #           'optimized': False,
+        #           'expression_csv': None,
+        #           },
+        # 'org26': {'genome_path': os.path.join(base_path, 'Peribacillus simplex.gb'),
+        #           'optimized': False,
+        #           'expression_csv': None,
+        #           },
+        # 'org27': {'genome_path': os.path.join(base_path, 'Phycicoccus duodecadis.gb'),
+        #           'optimized': False,
+        #           'expression_csv': None,
+        #           },
+        # 'org28': {'genome_path': os.path.join(base_path, 'Priestia flexa.gb'),
+        #           'optimized': False,
+        #           'expression_csv': None,
+        #           },
+        # 'org29': {'genome_path': os.path.join(base_path, 'Pseudarthrobacter phenanthrenivorans.gb'),
+        #           'optimized': False,
+        #           'expression_csv': None,
+        #           },
+        # 'org30': {'genome_path': os.path.join(base_path, 'Rhodanobacter denitrificans.gb'),
+        #           'optimized': False,
+        #           'expression_csv': None,
+        #           },
+        # 'org31': {'genome_path': os.path.join(base_path, 'Rhodanobacter fulvus.gb'),
+        #           'optimized': False,
+        #           'expression_csv': None,
+        #           },
+        # 'org32': {'genome_path': os.path.join(base_path, 'Terrabacter aerolatus.gb'),
+        #           'optimized': False,
+        #           'expression_csv': None,
+        #           },
+        # 'org33': {'genome_path': os.path.join(base_path, 'Terrabacter tumescens.gb'),
+        #           'optimized': False,
+        #           'expression_csv': None,
+        #           },
+        # 'org34': {'genome_path': os.path.join(base_path, 'Yonghaparkia alkaliphila.gb'),
+        #           'optimized': False,
+        #           'expression_csv': None,
+        #           },
     }
 }
 
@@ -176,31 +176,53 @@ def run_modules(user_input_dict: typing.Optional[typing.Dict[str, typing.Any]] =
         model_preferences_dict
     ) if model_preferences_dict is not None else models.ModelPreferences.init_from_config()
 
-    from modules.promoters.motif_filtering_after_streme import find_all_selective_files
-    from modules.promoters.run_meme import run_mast
-    from modules.promoters.globals_and_shared_methods import deopt_path
-
-    for index, selective_motif_file in enumerate(find_all_selective_files()):
-        splitted_motif_file_name = str(Path(selective_motif_file).parent.name).split("_")
-        # mast for first org
-        first_organism_name = "_".join(splitted_motif_file_name[:2])
-        second_organism_name = "_".join(splitted_motif_file_name[-4:-2])
-        logger.info(F"Run mast for first organism: {first_organism_name}")
-        first_org_promoter_dir = os.path.join(deopt_path, first_organism_name)
-        first_org_promoter_file_path = os.path.join(first_org_promoter_dir, F"{first_organism_name}_100_200.fasta")
-        run_mast(selective_motif_file, first_org_promoter_file_path, F"{first_organism_name}_{second_organism_name}_first")
-        # mast for second org
-        logger.info(F"Run mast for second organism: {second_organism_name}")
-        second_org_promoter_dir = os.path.join(deopt_path, second_organism_name)
-        second_org_promoter_file_path = os.path.join(second_org_promoter_dir, F"{second_organism_name}_100_200.fasta")
-        run_mast(selective_motif_file, second_org_promoter_file_path, F"{first_organism_name}_{second_organism_name}_second")
-
-    logger.info("The end")
-    exit(0)
-
     try:
-        # TODO - convert the input dictionary to a model we can pass to the other models
         input_dict = user_IO.UserInputModule.run_module(user_inp_raw)   # keys: sequence, selected_prom, organisms
+
+        import json
+        with open("parsed_input.json", "w") as user_input_file:
+            json.dump(input_dict, user_input_file)
+
+        import xml.etree.ElementTree as et
+        import csv
+
+        base_directory = os.path.join(artifacts_directory, "promoters_not_for_user")
+
+        for organism_name in input_dict["organisms"].keys():
+            organism_info = input_dict["organisms"][organism_name]
+            for deopt_org_name in input_dict["organisms"].keys():
+                if organism_name == deopt_org_name:
+                    logger.info(F"Skip run for organism {organism_name} and deopt organism {deopt_org_name}")
+                    continue
+
+                with open(F"opt_{organism_name}_deopt_{deopt_org_name}_first.csv", "w") as csv_file:
+                    csv_writer = csv.writer(csv_file)
+                    header = ["gene_name", "opt_organism_evalue", "opt_organism_cai_score"]
+                    csv_writer.writerow(header)
+
+                    opt_mast_file_name = F"mast {organism_name} {deopt_org_name} first\\mast.xml".replace(" ", "_")
+                    base_file = os.path.join(base_directory, opt_mast_file_name)
+                    tree = et.parse(base_file)
+                    root = tree.getroot()
+                    for m in root.findall(".//sequence"):
+                        seq_name = m.get("name")
+                        if "hypothetical" in seq_name:
+                            logger.info(F"skipping hypothetical match {seq_name}")
+                            continue
+                        promoter_score = m.find("score")
+                        promoter_evalue = promoter_score.get("evalue")
+                        # TODO - we can add here filtering of genes by their expression level, i.e. take only highly
+                        #  expressed genes
+                        matching_genes = [k for k in organism_info['cai_scores'].keys() if k.startswith(seq_name)]
+                        if len(matching_genes) > 1:
+                            logger.error(F"Found more than one matching gene for promoter! {matching_genes}")
+                            exit(1)
+                        cai_score = organism_info['cai_scores'][matching_genes[0]]
+                        data = [matching_genes[0], promoter_evalue,cai_score]
+                        csv_writer.writerow(data)
+
+        logger.info("The end!")
+        exit(0)
 
         ### unit 1 ############################################
         if model_preferences.restriction_enzymes or model_preferences.translation:
